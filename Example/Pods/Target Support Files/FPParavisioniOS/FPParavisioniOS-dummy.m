5a0048ed2c3ee71368561702eebd50f6c30965ca
Lucas Hubert <lucasjhubert@gmail.com>
2023-03-02 21:11:00 -0300

    complete setup

---
+++ Example/Pods/Target Support Files/FPParavisioniOS/FPParavisioniOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FPParavisioniOS : NSObject
+@end
+@implementation PodsDummy_FPParavisioniOS
+@end
---
+++ Example/Pods/Target Support Files/Pods-FPParavisioniOS_Example/Pods-FPParavisioniOS_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FPParavisioniOS_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FPParavisioniOS_Example
+@end
